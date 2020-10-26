cde50cb1492d6f515432262611a8a4e89cd1cecc
Ruan <28692837@student.g.nwu.ac.za>
2020-10-26 19:54:06 +0200

    FINAL DATABASES

---
+++ GroupProject223/GroupProject223/dbo.CLIENTS.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CLIENTS]
+(
+	[Client_ID] BIGINT NOT NULL PRIMARY KEY, 
+    [Booking_ID] INT NULL, 
+    [Client_Name] NCHAR(30) NULL, 
+    [Client_Surname] NCHAR(30) NULL, 
+    [Client_Email] NCHAR(30) NULL
+)
