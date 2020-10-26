01aa3b6e6c6673f4c1b2288fcf03494e763ffa10
Ruan <28692837@student.g.nwu.ac.za>
2020-10-26 19:54:06 +0200

    FINAL DATABASES

--- GroupProject223/GroupProject223/Form1.cs
+++ GroupProject223/GroupProject223/Form1.cs
@@ -16,5 +16,41 @@ namespace GroupProject223
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainUser m1 = new MainUser();
+            m1.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MaintainMenu m2 = new MaintainMenu();
+            m2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MaintainCompanies m3 = new MaintainCompanies();
+            m3.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MaintainDestination m4 = new MaintainDestination();
+            m4.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MaintainBooking m5 = new MaintainBooking();
+            m5.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
