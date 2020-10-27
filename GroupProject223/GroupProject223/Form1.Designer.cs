418d1c929d895df945c29815149d7ea1d606c0ae
Ruan <28692837@student.g.nwu.ac.za>
2020-10-27 21:23:56 +0200

    SELECT MEAL FORM BY

--- GroupProject223/GroupProject223/Form1.Designer.cs
+++ GroupProject223/GroupProject223/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,7 +44,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(267, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(255, 52);
+            this.label1.Size = new System.Drawing.Size(252, 51);
             this.label1.TabIndex = 0;
             this.label1.Text = "Admin page";
             // 
@@ -107,11 +108,22 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "Select the database you want to edit:";
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(56, 375);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -136,6 +148,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
     }
 }
 
